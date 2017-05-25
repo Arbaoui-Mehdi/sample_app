00d1d5b888608e462f08b98fe1a4da6feaff3486
Mehdi <whithat87@gmail.com>
2017-05-24 21:15:12 -0700

    Finish User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
+  # Home Page
   root             'static_pages#home'
+
+  #
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+
+  # Users
   get 'signup'  => 'users#new'
+  resources :users
 
 end
